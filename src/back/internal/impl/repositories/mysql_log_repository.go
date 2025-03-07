c828ce2c2044ac58263289cc90528b89c04fae8b
JoaoRafaVR <joao.rafael@vrsoft.com.br>
2025-03-07 17:38:16 -0300

    Atualiza dependências do sqlc e ajusta consultas de log

--- src/back/internal/impl/repositories/mysql_log_repository.go
+++ src/back/internal/impl/repositories/mysql_log_repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"time"
 
 	"github.com/ICEI-PUC-Minas-PCO-SI/2025-1-p3-tiapn-si-grupo-4/internal/store/store"
 )
@@ -10,7 +11,7 @@ type MySQLLogRepository struct {
 	q *store.Queries
 }
 
-func NewMysqlLogRepository(q *store.Queries)* MySQLLogRepository {
+func NewMysqlLogRepository(q *store.Queries) *MySQLLogRepository {
 	return &MySQLLogRepository{
 		q: q,
 	}
@@ -25,5 +26,23 @@ func (r *MySQLLogRepository) GetLogs(ctx context.Context) []store.Log {
 	return logs
 }
 func (r *MySQLLogRepository) Log(ctx context.Context, log store.Log) (store.Log, error) {
-	
+	result, err := r.q.Log(ctx, store.LogParams{
+		Servertime: time.Now(),
+		Log:        "Erro de conexão",
+		Level:      "error",
+		Source:     "servidor-01",
+	})
+	if err != nil {
+		return err
+	}
+
+	// Pega o ID inserido (só funciona com AUTO_INCREMENT)
+	lastID, _ := result.LastInsertId()
+
+	// Busca o registro completo
+	insertedLog, err := q.GetLog(ctx, lastID)
+	if err != nil {
+		return err
+	}
+
 }
--- src/back/internal/store/store/queries/queries.sql
+++ src/back/internal/store/store/queries/queries.sql
@@ -1,7 +1,15 @@
 -- name: GetLogs :many
-
 select * from logs;
 
--- name: Log :one 
+-- name: Log :execresult
+INSERT INTO logs (
+  id,
+  servertime,
+  log,
+  `level`,
+  source
+) VALUES (
+  ?, ?, ?, ?, ?
+);
 
-insert into logs (id, servertime, log, level, source ) values ($1, $2, $3, $4, $5) returning *;
+-- name 
