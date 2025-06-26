f0a0d54d8c979669e63fe8b24ee26de5cf8d322d
JoaoRafa19 <joaopedrorafael19@gmail.com>
2025-06-25 23:42:35 -0300

    chore: update project configuration to use Vite for building and development
    - Changed build and dev scripts in package.json to use Vite instead of react-router.
    - Added Vercel configuration file for routing.
    - Updated vite.config.ts to include Vercel plugin and set server port from environment variable.

--- src/front/vite.config.ts
+++ src/front/vite.config.ts
@@ -2,13 +2,19 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
 export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   plugins: [
     tailwindcss(), 
     reactRouter(), 
     tsconfigPaths(),
+    vercel(),
   ],
   resolve: {
     alias: {
